85ba1df54165087fc2fce658a817b7c883e31df0
Jens Tinglev <tinglev@kth.se>
2017-01-09 15:26:33 +0100

    Replaces debug and fatal with log and error

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 /*
-  Exposes the same function names as bunyan does.
-*/
+ * Exposes the same function names as bunyan does.
+ *
+ * Note that node console does not have a debug or fatal function, 
+ * so we'll use .log and .error instead instead
+ */
 
 module.exports = {
 
   trace: (msg) => console.trace(msg),
-  debug: (msg) => console.debug(msg),
+  debug: (msg) => console.log(msg),
   info: (msg) => console.info(msg),
   warn: (msg) => console.warn(msg),
   error: (msg) => console.error(msg),
-  fatal: (msg) => console.fatal(msg) 
+  fatal: (msg) => console.error(msg) 
 
 }
